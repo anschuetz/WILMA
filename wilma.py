ec35d8d63ad88ecb95b2360bd72c92874bb44bdf
Jesko Anschütz <jesko@linuxmuster.net>
2018-06-29 11:42:56 +0200

    TODO nur wenn nicht erledigt anzeigen

--- wilma.py
+++ wilma.py
@@ -48,13 +48,13 @@ inhalt = htmlkopf + htmlbody
 c = Calendar(requests.get(url).text)
 try:
         for t in c.todos:
-                div = "eintrag"
-                print("------------------------------------------------------------")
-                print("-- {} -- Erzeuge Todo '{}'".format(now, t.name))
-                print("-- RAW-Data: {}".format(t))
-                inhalt+='<div class="'+div+'">'
-                inhalt+='<div class="titel">' + t.name + '</div>'
-                inhalt+="</div>"
+                if t.percent < 100:
+                        div = "eintrag"
+                        print("------------------------------------------------------------")
+                        print("-- {} -- Erzeuge Todo '{}', {}% erledigt".format(now, t.name, t.percent))
+                        inhalt+='<div class="'+div+'">'
+                        inhalt+='<div class="titel">' + t.name + '</div>'
+                        inhalt+="</div>"
 except all:
         print("Todos lesen klappt nicht")
 try:
