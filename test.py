9f0e849601ec775e9e3f135601129b75fa914c58
Jesko Anschütz <jesko@linuxmuster.net>
2018-10-15 16:29:41 +0200

    bereinigungen

--- test.py
+++ test.py
@@ -1,27 +1,9 @@
 # -*- coding: utf-8 -*-
 debugflag = True
-
-def fileExists(filename):
-  try:
-          f = open(filename)
-          f.close()
-          return True
-  except IOError as detail:
-          print(detail)
-          return False
-  except Exception as detail:
-          print("unbehandelter Fehler in Funktion 'fileExists'", detail)
-          exit(1)
-
-def debugprint(message):
-        if debugflag:
-                print(message)
-
-
-
-open("asf")
-print("oberster try-block")
-debugprint("Debugmodus an")
-print(fileExists("testpy"))
-print(fileExists("test.py"))
-        
+try:
+   print("das ist der erste Block, automatisch eingerückt")
+   print("das ist mit spaces eingerückt")
+   print("das war ein TAB")
+except Exception:
+   print("schiefgelaufne...")
+print("ohne Einrückung")
